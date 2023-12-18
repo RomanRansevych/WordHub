c40e76b661abbf093ff340900ecf06bc512d1b0c
Roman Ransevych <ransevych@gmail.com>
2023-12-18 15:32:10 +0200

    Empty project with connection to DB

---
+++ src/test/java/com/company/wordhub/WordHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.wordhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
